4e4c350256034bec173cf8b357b4c3f4d9fcd31b
Shoaib <shoaib.salim@npstx.com>
2023-07-31 13:58:41 +0530

    Update counter-output component to use async pipe instead of subscription.

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,29 +1,18 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { counterDTO } from '../state/counter.state';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter!: number;
-
-  // use subsription
-  counterSubs!: Subscription;
+export class CounterOutputComponent implements OnInit {
+  counter$!: Observable<any>;
 
   constructor(private store: Store<counterDTO>) {}
-
   ngOnInit(): void {
-    this.counterSubs = this.store.select('counter').subscribe((data: any) => {
-      this.counter = data.counter;
-    });
-  }
-  ngOnDestroy(): void {
-    if (this.counterSubs) {
-      this.counterSubs.unsubscribe();
-    }
+    this.counter$ = this.store.select('counter');
   }
 }
